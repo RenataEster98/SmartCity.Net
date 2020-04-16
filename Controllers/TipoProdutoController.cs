a455ee606de90846a9ac8381c9a5b85960dc713e
Renata <ester.b@live.com>
2020-04-16 01:03:42 -0300

    First Commit

---
+++ Controllers/TipoProdutoController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FiapSmartCity.Controllers
+{
+    public class TipoProdutoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/TipoProduto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FiapSmartCity.Models
+{
+    public class TipoProduto
+    {
+        public int IdTipo { get; set; }
+        public String DescricaoTipo { get; set; }
+        public bool Comercializado { get; set; }
+    }
+}
